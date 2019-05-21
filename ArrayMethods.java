ce39a907c8af9b4c6cb88ced7a58a4eb19690d3b
jacquiha <47220557+jacquiha@users.noreply.github.com>
2019-05-20 20:56:13 -0400

    Add files via upload

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -2,10 +2,10 @@
 public class ArrayMethods {
 	public static int sum(int[] arr, int firstIndex, int lastIndex) {
 		int sum = 0;
+		if ((firstIndex < 0) || (firstIndex > lastIndex) || (firstIndex > arr.length) || (lastIndex < 0) || (lastIndex > arr.length)) {
+			return -666; }
 		for (int i = 0; i < arr.length; i++) {
 			sum = sum + arr[i];
-			if ((firstIndex < 0) || (firstIndex > arr.length) || (lastIndex < 0) || (lastIndex > arr.length)) {
-				return -666; }
 		}
 		return sum;
 	}
@@ -13,41 +13,36 @@ public class ArrayMethods {
 	public static double average(int[] arr, int firstIndex, int lastIndex) {
 		double average = 0.0;
 		double sum = 0.0;
+		if ((firstIndex < 0) || (firstIndex > lastIndex) || (firstIndex > arr.length) || (lastIndex < 0) || (lastIndex > arr.length)) {
+			return -666.0; }
 		for (int i=0; i < arr.length; i++) {
 			sum = sum + arr[i];
-			average = sum/arr.length;
-			if ((firstIndex < 0) || (firstIndex > arr.length) || (lastIndex < 0) || (lastIndex > arr.length)) {
-				return -666.0; }
 		}
-		return average;
+		return sum/arr.length;
 	}
 	
 	public static int belowAverage(int[] arr, int firstIndex, int lastIndex) {
 		int counter = 0;
-		int average = 0;
+		double average = 0.0;
 		int sum = 0;
+		if ((firstIndex < 0) || (firstIndex > lastIndex) || (firstIndex > arr.length) || (lastIndex < 0) || (lastIndex > arr.length)) {
+			return -666; }
 		for (int i=0; i < arr.length; i++) {
 			sum = sum + arr[i];
 			average = sum/arr.length;
-			if (arr[i] < average) counter++;
-			if ((firstIndex < 0) || (firstIndex > arr.length) || (lastIndex < 0) || (lastIndex > arr.length)) {
-				return -666; 
-				}
+			if (arr[i] < average) counter++; 
 	}
 		return counter;
 }
 	public static int aboveAverage(int[] arr, int firstIndex, int lastIndex) {
 		int counter = 0;
-		int average = 0;
-		int sum = 0;
+		double average = 0.0;
 		for (int i=0; i < arr.length; i++) {
-			sum = sum + arr[i];
-			average = sum/arr.length;
-			if (arr[i] > average) counter++;
-			if ((firstIndex < 0) || (firstIndex > arr.length) || (lastIndex < 0) || (lastIndex > arr.length)) {
-				return -666; 
-				}
-	}
+		average = average(arr, firstIndex, lastIndex);
+		if (arr[i] > average) counter++;
+		}
+		if ((firstIndex < 0) || (firstIndex > lastIndex) || (firstIndex > arr.length) || (lastIndex < 0) || (lastIndex > arr.length)) {
+			return -666; }
 		return counter;
 	}
 	
